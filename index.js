2a21d693cc41fc7376652f4a701833394a935a8b
Marcia Hernández <ht.marcia@gmail.com>
2023-05-17 19:51:51 -0600

    Deploy configs

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 // Syncing all the models at once.
 conn
   .sync({ force: true })
   .then(() => {
     server.listen(PORT, () => {
-      console.log("Listening at 3001"); // eslint-disable-line no-console
+      console.log(`Listening at ${3001}`); // eslint-disable-line no-console
     });
   })
   .catch((err) => console.log(err));
